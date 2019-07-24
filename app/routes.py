a3edf5365478b84919993140a97d1562efe4caa6
EC2 Default User <ec2-user@ip-172-31-22-105.us-east-2.compute.internal>
2019-07-24 14:06:39 +0000

    new commit

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,7 @@ from app.models import model, formopener
 
 @app.route('/')
 @app.route('/index')
-def index():
-    user = {'name':'Jeffron', 'favGame':'Debugger' }
-    return render_template('index.html', user=user)
+
 
 @app.route('/secret')
 def secret():
@@ -22,7 +20,7 @@ def sendBreakfast():
         return "You didn't fill out the form and I bet you say 'routes' weirdly"
     else:
         userData = dict(request.form)
-        nickname = userData["nickname"][0]
-        breakfast = userData["breakfast"][0]
+        nickname = userData["nickname"]
+        breakfast = userData["breakfast"]
         return render_template("breakfast.html", nickname = model.shout(nickname), breakfast = model.shout(breakfast))
         
